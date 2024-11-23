7925f968add59348f4f8d3ffb3035c4d53c6df9d
dilarakiraz <dilarakiraz12345@gmail.com>
2024-11-24 01:29:55 +0300

    setup compose image loader

--- composeApp/src/commonMain/kotlin/org/dilarakiraz/composempnotes/App.kt
+++ composeApp/src/commonMain/kotlin/org/dilarakiraz/composempnotes/App.kt
@@ -2,14 +2,27 @@ package org.dilarakiraz.composempnotes
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -20,16 +33,53 @@ import ecommercekmm.composeapp.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
+
+    }
+}
+
+@Composable
+fun AppContent(homeViewModel: HomeViewModel) {
+
+    val products = homeViewModel.products.collectAsState()
+
+    BoxWithConstraints {
+        val scope = this
+        val maxWidth = scope.maxWidth
+
+        var cols = 2
+        var modifier = Modifier.fillMaxWidth()
+        if (maxWidth > 840.dp) {
+            cols = 3
+            modifier = Modifier.widthIn(max = 1080.dp)
+        }
+
+        val scrollState = rememberLazyGridState()
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(cols),
+                state = scrollState,
+                contentPadding = PaddingValues(16.dp)
+            ) {
+                items(
+                    items = products.value,
+                    key = { product -> product.id.toString() }) { product ->
+                    Card(
+                        shape = RoundedCornerShape(15.dp),
+                        modifier = Modifier.padding(8.dp)
+                            .fillMaxWidth(),
+                        elevation = 2.dp
+                    ) {
+                        Column (
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ){
+
+                        }
+                    }
                 }
             }
         }
